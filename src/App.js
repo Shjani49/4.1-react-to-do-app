72e841f80ac25ac8492b04fb5a08d34f062c52c1
Shjani49 <shjani49@gmail.com>
2020-07-27 10:14:37 -0600

    Add 'new to-do' form JSX to App.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ class App extends React.Component
   render ()
   {
     return(
-        <h1>Hello Shivani!</h1>
+      <>
+        <h1>React To-Do App</h1>
+        <form>
+          <label htmlFor = "newToDo">
+            Enter a New "To-Do" :
+            <input type ="text" name = "newToDo" id = "newToDo" required />
+          </label>
+          <input type = "submit" value = "Add New To-Do" />
+        </form>
+        <h2>Current To-Dos:</h2>
+        <ul>
 
-
+        </ul>
+    </>
     );
   }
 
