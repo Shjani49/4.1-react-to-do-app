69bdeface3eb0831ac5dfb9357e08929a82a58c6
Shjani49 <shjani49@gmail.com>
2020-07-27 11:11:05 -0600

    Add updateItem() method; add setState() example (newToDo.)

--- src/App.js
+++ src/App.js
@@ -19,8 +19,16 @@ class App extends React.Component
   addToDo ( event )
   {
     event.preventDefault();
-    console.log(" test add tod do");
+    console.log( "test add todo!" );
 
+  }
+
+  updateItem ( key, value )
+  { // We never re-assign the contents of this.state.
+    // this.state is ONLY USED FOR READING VALUES, NOT writing.
+    // when we need to update anything in state, we need to use this.setState()
+    // 
+    this.setState ( {[key] : value} );
   }
 
   render ()
@@ -32,8 +40,15 @@ class App extends React.Component
         <form onSubmit ={this.addToDo}>
           <label htmlFor = "newToDo">
             Enter a New "To-Do" :
-            <input type ="text" name = "newToDo" id = "newToDo" required />
+            <input 
+            type ="text" 
+            name = "newToDo" 
+            id = "newToDo" 
+            required  
+            value = {this.state.newToDo} 
+            onChange ={event => this.updateItem( 'newToDo', event.target.value )}/>
           </label>
+
           <input type = "submit" value = "Add New To-Do" />
         </form>
         <h2>Current To-Dos:</h2>
